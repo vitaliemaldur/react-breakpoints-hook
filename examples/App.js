a7101ae575cb27042a9fb6705d548d65918e1db4
Vitalie Maldur <maldur.vitalie@gmail.com>
2020-01-08 17:37:06 +0200

    Updated package.json

--- examples/App.js
+++ examples/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { useBreakpoints, useCurrentWitdh } from '../src/lib';
+import { useBreakpoints, useCurrentWitdh } from 'react-breakpoints-hook';
 
 
 const App = () => {
   let width = useCurrentWitdh();
-  let { xs, sm, md, lg, smmd } = useBreakpoints({
+  let { xs, sm, md, lg } = useBreakpoints({
     xs: {min: 0, max: 360},
     sm: {min: 361, max: 960},
     md: {min: 961, max: 1400},
     lg: {min: 1401, max: null},
-    smmd: {min: 400, max: 1300},
   });
 
   return (
@@ -18,7 +17,7 @@ const App = () => {
         {`Current width -> ${width}`}
       </h1>
       <p>
-        {`Breakpoint: xs(${xs}) sm(${sm}) md(${md}) lg(${lg}), smmd(${smmd})`}
+        {`Breakpoint: xs(${xs}) sm(${sm}) md(${md}) lg(${lg})`}
       </p>
     </div>
   );
